2c41eda3b2bf1b4a7f448a17be334884a73fbb41
Daniel Abrecht <public@danielabrecht.ch>
2022-09-07 20:31:27 +0000

    Some more work on serializer

--- src/simple-tree-serializer.c
+++ src/simple-tree-serializer.c
@@ -8,6 +8,7 @@
 #define SERIALIZER_STATE \
   X(SERIALIZER_W_START) \
   X(SERIALIZER_W_DONE) \
+  X(SERIALIZER_W_NEXT_CHILD) \
   X(SERIALIZER_W_NEXT_MEMBER) \
   X(SERIALIZER_W_TAG_START) \
   X(SERIALIZER_W_TAG) \
@@ -47,6 +48,7 @@ struct ml666_st_serializer_private {
   struct ml666_st_serializer public;
   struct ml666_st_node* node;
   int fd;
+  unsigned level, spaces;
 
   struct ml666_st_node* cur;
   enum serializer_state state;
@@ -104,20 +106,26 @@ struct ml666_st_serializer* ml666_st_serializer_create_p(struct ml666_st_seriali
 
 bool ml666_st_serializer_next(struct ml666_st_serializer* _sts){
   struct ml666_st_serializer_private*restrict sts = (struct ml666_st_serializer_private*)_sts;
-  sts->public.error = "TODO: This isn't fully implemented yet";
-  return false;
   while(sts->state != SERIALIZER_W_DONE){
     if(sts->data.length || sts->outptr.length){
       struct ml666_buffer_ro buf = sts->data;
       struct ml666_buffer outbuf = sts->outbuf;
       size_t i=0, j=sts->outptr.data-outbuf.data+sts->outptr.length;
-      if(buf.length){
+      if(sts->spaces){
+        size_t l = sts->spaces;
+        if(l > outbuf.length-j)
+          l = outbuf.length-j;
+        memset(outbuf.data+j, ' ', l);
+        sts->spaces -= l;
+        j += l;
+      }
+      if(!sts->spaces && buf.length){
         switch(sts->encoding){
           case ENC_RAW: {
             size_t l = buf.length;
-            if(l > outbuf.length)
-              l = outbuf.length;
-            memcpy(outbuf.data, buf.data, l);
+            if(l > outbuf.length-j)
+              l = outbuf.length-j;
+            memcpy(outbuf.data+j, buf.data, l);
             i += l;
             j += l;
           } break;
@@ -178,19 +186,25 @@ bool ml666_st_serializer_next(struct ml666_st_serializer* _sts){
       switch(sts->state){
         case SERIALIZER_W_DONE: break;
         case SERIALIZER_W_START: switch(ML666_ST_TYPE(sts->cur)){
-          case ML666_ST_NT_DOCUMENT: sts->state = SERIALIZER_W_NEXT_MEMBER; break;
+          case ML666_ST_NT_DOCUMENT: sts->state = SERIALIZER_W_NEXT_CHILD; break;
           case ML666_ST_NT_ELEMENT : sts->state = SERIALIZER_W_TAG_START; break;
           case ML666_ST_NT_CONTENT : sts->state = SERIALIZER_W_CONTENT_START; break;
           case ML666_ST_NT_COMMENT : sts->state = SERIALIZER_W_COMMENT_START; break;
         } break;
-        case SERIALIZER_W_NEXT_MEMBER: {
+        case SERIALIZER_W_NEXT_CHILD: {
           struct ml666_st_children* children = ML666_ST_U_CHILDREN(sts->public.stb, sts->cur);
           struct ml666_st_member* first_child = children ? ml666_st_get_first_child(sts->public.stb, children) : 0;
           sts->state = SERIALIZER_W_DONE;
           if(first_child){
+            if(ML666_ST_TYPE(sts->cur) == ML666_ST_NT_ELEMENT)
+              sts->level += 1;
             sts->cur = ML666_ST_NODE(first_child);
             sts->state = SERIALIZER_W_START;
-          }else if(sts->cur != sts->node){
+          }
+        } break;
+        case SERIALIZER_W_NEXT_MEMBER: {
+          sts->state = SERIALIZER_W_DONE;
+          if(sts->cur != sts->node){
             struct ml666_st_member* member = ML666_ST_U_MEMBER(sts->cur);
             struct ml666_st_member* next_child = member ? ml666_st_member_get_next(sts->public.stb, member) : 0;
             if(next_child){
@@ -201,11 +215,14 @@ bool ml666_st_serializer_next(struct ml666_st_serializer* _sts){
               if(parent && parent != sts->node && ML666_ST_TYPE(parent) == ML666_ST_NT_ELEMENT){
                 sts->cur = parent;
                 sts->state = SERIALIZER_W_END_TAG_START;
+                if(sts->level)
+                  sts->level -= 1;
               }
             }
           }
         } break;
         case SERIALIZER_W_TAG_START: {
+          sts->spaces = sts->level * 2;
           sts->data.data = "<";
           sts->data.length = 1;
           sts->state = SERIALIZER_W_TAG;
@@ -227,13 +244,15 @@ bool ml666_st_serializer_next(struct ml666_st_serializer* _sts){
           if(ml666_st_get_first_child(sts->public.stb, ML666_ST_U_CHILDREN(sts->public.stb, sts->cur))){
             sts->data.data = ">\n";
             sts->data.length = 2;
+            sts->state = SERIALIZER_W_NEXT_CHILD;
           }else{
             sts->data.data = "/>\n";
             sts->data.length = 3;
+            sts->state = SERIALIZER_W_NEXT_MEMBER;
           }
-          sts->state = SERIALIZER_W_NEXT_MEMBER;
         } break;
         case SERIALIZER_W_END_TAG_START: {
+          sts->spaces = sts->level * 2;
           sts->data.data = "</";
           sts->data.length = 2;
           sts->state = SERIALIZER_W_END_TAG;
