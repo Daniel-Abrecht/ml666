61e5cf20b220ffa235198d2ce6130b4a21a26cdc
Daniel Abrecht <public@danielabrecht.ch>
2022-10-02 12:30:45 +0000

    Update the ml666.h convenience header to include all headers

--- include/ml666/ml666.h
+++ include/ml666/ml666.h
@@ -1,8 +1,15 @@
 #ifndef ML666_H
 #define ML666_H
 
-#include <ml666/tokenizer.h>
-#include <ml666/parser.h>
 #include <ml666/utils.h>
+#include <ml666/common.h>
+#include <ml666/parser.h>
+#include <ml666/tokenizer.h>
+#include <ml666/simple-tree.h>
+#include <ml666/simple-tree-parser.h>
+#include <ml666/simple-tree-builder.h>
+#include <ml666/simple-tree-serializer.h>
+#include <ml666/simple-tree-json-serializer.h>
+#include <ml666/simple-tree-ml666-serializer.h>
 
 #endif
